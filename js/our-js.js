31ea84fca1f435b33eca3e89859fff0aabd72473
maddiegabriel <maddie.gabes@gmail.com>
2020-12-05 17:33:50 -0500

    added all new pages; moved demo stuff to /demos

--- js/our-js.js
+++ js/our-js.js
@@ -4,10 +4,10 @@ $("body").toggleClass("sidebar-toggled");
 $(".sidebar").toggleClass("toggled");
 if ($(".sidebar").hasClass("toggled")) {
     $('.sidebar .collapse').collapse('hide');
-    document.getElementById("our-logo").src = "img/our-logo-collapsed.png";
+    document.getElementById("our-logo").src = "/thedigitaldivide/img/our-logo-collapsed.png";
     document.getElementById("our-logo").style.maxWidth = "3rem";
 } else {
-    document.getElementById("our-logo").src = "img/our-logo.png";
+    document.getElementById("our-logo").src = "/thedigitaldivide/img/our-logo.png";
     document.getElementById("our-logo").style.maxWidth = "14rem";
 }
 });
