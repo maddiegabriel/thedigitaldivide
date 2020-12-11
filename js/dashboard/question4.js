c1a487791acaa9f31c99657029d643ec04a72240
maddiegabriel <maddie.gabes@gmail.com>
2020-12-11 16:48:05 -0500

    lit review

--- js/dashboard/question4.js
+++ js/dashboard/question4.js
@@ -2,7 +2,7 @@
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
-// Bar Chart Example
+// Bar Chart QUESTION 4
 var ctx = document.getElementById("q4BarChart");
 var myBarChart = new Chart(ctx, {
     type: 'bar',
@@ -58,6 +58,84 @@ var myBarChart = new Chart(ctx, {
         },
         legend: {
             display: false
+        },
+        tooltips: {
+            titleMarginBottom: 10,
+            titleFontColor: '#6e707e',
+            titleFontSize: 14,
+            backgroundColor: "rgb(255,255,255)",
+            bodyFontColor: "#858796",
+            borderColor: '#dddfeb',
+            borderWidth: 1,
+            xPadding: 15,
+            yPadding: 15,
+            displayColors: false,
+            caretPadding: 10,
+            callbacks: {
+                label: function(tooltipItem) {
+                    return tooltipItem.yLabel + ' study hours';
+                }
+            }
+        },
+    }
+});
+
+// Bar Chart QUESTION 5
+var ctx5 = document.getElementById("q5BarChart");
+var myBarChart5 = new Chart(ctx5, {
+    type: 'bar',
+    data: {
+        labels: ["Low-Income", "Non Low-Income"],
+        datasets: [{
+            label: "Study Time",
+            backgroundColor: "#4e73df",
+            hoverBackgroundColor: "#2e59d9",
+            borderColor: "#4e73df",
+            data: [5.36, 5.525],
+        }],
+    },
+    options: {
+        maintainAspectRatio: false,
+        layout: {
+            padding: {
+                left: 10,
+                right: 25,
+                top: 25,
+                bottom: 0
+            }
+        },
+        scales: {
+            xAxes: [{
+                time: {
+                    unit: 'month'
+                },
+                gridLines: {
+                    display: false,
+                    drawBorder: false
+                },
+                ticks: {
+                    maxTicksLimit: 6
+                },
+                maxBarThickness: 25,
+            }],
+            yAxes: [{
+                ticks: {
+                    min: 0,
+                    max: 6,
+                    maxTicksLimit: 10,
+                    padding: 10,
+                },
+                gridLines: {
+                    color: "rgb(234, 236, 244)",
+                    zeroLineColor: "rgb(234, 236, 244)",
+                    drawBorder: false,
+                    borderDash: [2],
+                    zeroLineBorderDash: [2]
+                }
+            }],
+        },
+        legend: {
+            display: true
         },
         tooltips: {
             titleMarginBottom: 10,
